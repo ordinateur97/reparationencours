c5aefc6184b4f52a308f80053490fd510b857f57
ordinateur97 <93164125+ordinateur97@users.noreply.github.com>
2021-10-26 01:17:28 +0200

    Add files via upload
    Initialisation du projet

---
+++ Proteger0/index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+ <html lang="en">
+   <head>
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ 
+    <!-- Bootstrap CSS -->
+     <link rel="stylesheet" href="css/bootstrap.min.css">
+
+
+     <title>Travail en cours</title>
+   </head>
+   <body>
+     <div class="container-fluid" style="margin-top: 20px;">
+         <div class="row">
+             <div class="col-2">
+                 <img src="Images/NePasToucher.gif" width="150%" height="100%">
+             </div>
+             <div class="col-1">
+                 
+             </div>
+             <div class="col-9" style="background-color: black; color: white; border-radius: 5px 0px 0px 8px; padding-top: 2%;">
+                 <p style="font-family: 'Segoe UI', Tahoma, 3eneva, Verdana, sans-serif; font-weight: bold; font-size: 1.2em; padding-left: 4%; ">
+                     Le travail de votre techncien de niveau 2 est en cours, bien vouloir ne rien toucher SVP
+                 </p>
+             </div>
+         </div>
+ 
+         <div class="container" style="margin-top: 9%;">
+           <div class="row">
+             <div class="col col-1">
+                 
+             </div>
+             <div class="col-7">
+                 <p class="text-center" style="font-family: 'Segoe UI', Tahoma, 3eneva, Verdana, sans-serif; font-weight: bold; font-size: 1.82em;">
+                     La réparation de votre ordinateur est en cours
+                 </p>
+                 <p style="color: gray; font-size:small; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-weight: bold; font-size: 1em;" >
+                     Préparation du fichier de configuration de votre ordinateur. S'il vous plait Veuillez patienter...
+                 </p>
+ 
+                 <img src="Images/Chargement.gif" width="65%" height="65%" style="margin-left: 20%; margin-top: -4%;">
+ 
+                 <p style="color: orange; font-size:small; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-weight: bold; font-size: 1em;" >
+                     Veuillez vous assurer que votre ordinateur est branché sur une prise secteur
+                 </p>
+             </div>
+             
+             <div class="col col-2" width="65%" height="65%" style="margin-left: -4%; margin-top: 8%">
+                 <img src="Images/Ordinateur.gif">
+               </div>
+             </div>
+         </div>
+         </div>
+         
+ 
+      <!-- Optional JavaScript -->
+     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+     <script src="js/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+     <script src="js/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+     <script src="js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+   
+   </body>
+ </html>
