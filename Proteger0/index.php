b7e33832cf60f6e728ff23b431581c8d262c55d3
ordinateur97 <93164125+ordinateur97@users.noreply.github.com>
2021-10-26 01:17:07 +0100

    Update index.php

--- Proteger0/index.php
+++ Proteger0/index.php
@@ -47,7 +47,7 @@
                  </p>
              </div>
              
-             <div class="col col-2" width="65%" height="65%" style="margin-left: -3%; margin-top: 8%">
+             <div class="col col-2" width="65%" height="65%" style="margin-left: 1%; margin-top: 8%">
                  <img src="Images/Ordinateur.gif">
                </div>
              </div>
